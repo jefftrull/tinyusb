e65341b0f4e9c3b8bd034a35d7b5f2e116167462
Jeff Trull <edaskel@att.net>
2023-02-13 14:05:19 -0800

    Add source files for emulated riscv on 8051 example

---
+++ hw/bsp/cs4200f/board.mk
@@ -0,0 +1,19 @@
+CFLAGS += \
+  -DTUP_DCD_ENDPOINT_MAX=8 \
+  -march=rv32im -mabi=ilp32 \
+  -nostdlib \
+  -nostartfiles \
+  -nodefaultlibs \
+  -fno-builtin \
+  -ffreestanding \
+  -Wno-unused-parameter
+
+CROSS_COMPILE = riscv32-unknown-elf-
+
+ASFLAGS = -march=rv32im
+
+MCU_DIR = hw/mcu/ali/m56xx
+
+SRC_C += src/portable/ali/m56xx/dcd_m56xx_rv51.c
+
+INC +=  $(TOP)/$(MCU_DIR)
