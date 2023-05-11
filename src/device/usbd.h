746af17ae72f62c44169b1e8bb0d880b9e2db5b4
Jeff Trull <edaskel@att.net>
2023-05-10 20:00:33 -0700

    Update a couple of callbacks to use XDATA instead of generic pointers
    Not sure if this was the source of the problem I was having (set line
    control failing with a stall) but it certainly fixed it and generics
    are unnecessary here.

--- src/device/usbd.h
+++ src/device/usbd.h
@@ -145,7 +145,7 @@ TU_ATTR_WEAK void tud_suspend_cb(bool remote_wakeup_en);
 TU_ATTR_WEAK void tud_resume_cb(void);
 
 // Invoked when received control request with VENDOR TYPE
-TU_ATTR_WEAK bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const * request);
+TU_ATTR_WEAK bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, __xdata tusb_control_request_t const * request);
 
 //--------------------------------------------------------------------+
 // Binary Device Object Store (BOS) Descriptor Templates
